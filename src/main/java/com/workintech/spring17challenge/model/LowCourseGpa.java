dde5ee192caf4e2838ec78b3280ef82da15e62b4
ozgur-catal <168709108+ozgur-catal@users.noreply.github.com>
2024-10-21 17:52:59 +0300

    Sprint 17 Challenge Görev Bitti

---
+++ src/main/java/com/workintech/spring17challenge/model/LowCourseGpa.java
@@ -0,0 +1,12 @@
+
+package com.workintech.spring17challenge.model;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class LowCourseGpa implements CourseGpa{
+    @Override
+    public int getGpa() {
+        return 3;
+    }
+}
